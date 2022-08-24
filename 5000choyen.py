d61fabaa7602298b6c03384a49dd7eec0a26346d
Yidaozhan Ya <yidaozhanyaqwq@gmail.com>
2022-08-24 15:31:31 +0800

    cli

--- 5000choyen.py
+++ 5000choyen.py
@@ -5,7 +5,7 @@ import sys
 from generator import genImage
 
 if '-h' in sys.argv or '--help' in sys.argv:
-    print('Usage: 500choyen [上半句] [下半句] [输出文件]')
+    print('Usage: 5000choyen [上半句] [下半句] [输出文件]')
     exit()
 img=genImage(word_a=sys.argv[1], word_b=sys.argv[2])
 img.save(sys.argv[3])
