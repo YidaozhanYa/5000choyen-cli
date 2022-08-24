1d9a9169ebad8316a02bd0cd99aaaded42e07a6c
Yidaozhan Ya <yidaozhanyaqwq@gmail.com>
2022-08-24 14:55:52 +0800

    cli

---
+++ 500choyen.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+from PIL import Image
+import sys
+from generator import genImage
+
+if '-h' in sys.argv or '--help' in sys.argv:
+    print('Usage: 500choyen [上半句] [下半句] [输出文件]')
+    exit()
+img=genImage(word_a=sys.argv[1], word_b=sys.argv[2])
+img.save(sys.argv[3])
